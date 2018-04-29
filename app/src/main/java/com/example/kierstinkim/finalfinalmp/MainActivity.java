60f433f2fcd03421ff316ea3467ee76b7683ca23
Kierstin Kim <seyeonk2@illinois.edu>
2018-04-29 06:25:02 -0500

    Restaurants' name should be up by clicking each button, this can be done by changing SEND call Couldn't get API work

--- app/src/main/java/com/example/kierstinkim/finalfinalmp/MainActivity.java
+++ app/src/main/java/com/example/kierstinkim/finalfinalmp/MainActivity.java
@@ -8,6 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,14 +26,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         final ImageView imageView = findViewById(R.id.imageView);
+        final Button button1 = findViewById(R.id.button1);
+        final Button button2 = findViewById(R.id.button2);
+        final Button button3 = findViewById(R.id.button3);
+        final Button button4 = findViewById(R.id.button4);
+        final Button button5 = findViewById(R.id.button5);
+        final Button button6 = findViewById(R.id.button6);
         final TextView textView = findViewById(R.id.textView);
-        final TextView textView2 = findViewById(R.id.textView2);
-        final Button mapButton = findViewById(R.id.button);
+        final Button button = findViewById(R.id.button);
 
     }
 
     public void browser(View view) {
-        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.windy.com/?40.078,-88.198,5"));
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.yelp.com/"));
         startActivity(browserIntent);
     }
+
+    RequestQueue queue = Volley.newRequestQueue(this);
+
 }
+
+
