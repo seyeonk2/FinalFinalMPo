ed46fa85e7ccb5c2c138d1366602ba59881865b5
Yunkai Zhu <yunkaiz2@illinois.edu>
2018-04-30 20:40:10 -0500

    build failed,API key doesn't work

--- app/src/main/java/com/example/kierstinkim/finalfinalmp/MainActivity.java
+++ app/src/main/java/com/example/kierstinkim/finalfinalmp/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,13 +13,23 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
+
+    /** Default logging tag for messages from the main activity. */
+    private static final String TAG = "FinalFinalMPo:Main";
+
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +38,53 @@ public class MainActivity extends AppCompatActivity {
 
         final ImageView imageView = findViewById(R.id.imageView);
         final Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "American button clicked");
+                startAPICall();
+            }
+        });
         final Button button2 = findViewById(R.id.button2);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "American button clicked");
+                startAPICall();
+            }
+        });
         final Button button3 = findViewById(R.id.button3);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "American button clicked");
+                startAPICall();
+            }
+        });
         final Button button4 = findViewById(R.id.button4);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "American button clicked");
+                startAPICall();
+            }
+        });
         final Button button5 = findViewById(R.id.button5);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "American button clicked");
+                startAPICall();
+            }
+        });
         final Button button6 = findViewById(R.id.button6);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                Log.d(TAG, "American button clicked");
+                startAPICall();
+            }
+        });
         final TextView textView = findViewById(R.id.textView);
         final Button button = findViewById(R.id.button);
 
@@ -44,6 +97,49 @@ public class MainActivity extends AppCompatActivity {
 
     RequestQueue queue = Volley.newRequestQueue(this);
 
+    void startAPICall() {
+        try {
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.GET,
+                    "https://api.yelp.com/v3/businesses/search.API_KEY"
+                            + BuildConfig.API_KEY,
+                    null,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(final JSONObject response) {
+                            try {
+                                Log.d(TAG, response.toString(2));
+                            } catch (JSONException ignored) { }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(final VolleyError error) {
+                    Log.e(TAG, error.toString());
+                }
+            });
+            requestQueue.add(jsonObjectRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    private String getAmerican() {
+            return "none";
+    }
+    private String getChinese() {
+        return "none";
+    }
+    private String getItalian() {
+        return "none";
+    }
+    private String getKorean() {
+        return "none";
+    }
+    private String getMexican() {
+        return "none";
+    }
+    private String getThai() {
+        return "none";
+    }
 }
 
 
